42514111cea232d524c130fa21de43f5febba1a4
DhruvvGitHub <dhruvtanwani1915@gmail.com>
2024-07-23 00:35:00 +0530

    updated code where 2 people can chat working fine

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const userids = []
+const usernames = []
+
+io.on("connection", (socket) => {
+    socket.on("message", function (message) {
+        let index = userids.indexOf(socket.id);
+        let name = usernames[index];
+        io.emit("message", { message, name, id:socket.id });
+    });
+    
+    socket.on("nameset", (username) => {
+        if (username.length === 0) {
+            socket.emit("nameEmptyError")
+        }
+        else if (username.length < 3) {
+            socket.emit("nameSmallError")
+        } 
+        else {
+            userids.push(socket.id)
+            usernames.push(username);
+
+            socket.emit("namesetdone")
+
+            io.emit("numberofliveusers", usernames.length)
+        }
+        
+
+    })
+
+    socket.on("disconnect", () => {
+        let index = userids.indexOf(socket.id)
+
+        if(index !== -1) {
+            usernames.splice(index, 1)
+            userids.splice(index, 1)
+        }
+
+        io.emit("numberofliveusers", usernames.length)
+    })
+});
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.listen(3000);
---
+++ public/javascripts/script.js
@@ -0,0 +1,103 @@
+// Building a Chatting App 
+
+const socket = io();
+
+const nameInput = document.querySelector("#name-input");
+const username = document.querySelector(".username")
+const nameSubmit = document.querySelector("#name-submit");
+const input = document.querySelector("#input-area");
+const sendButton = document.querySelector("#send-button");
+const messageDiv= document.querySelector("#message-div"); 
+const messageBox = document.querySelector("#messages-box");
+const nameScreen = document.querySelector("#name-screen")
+const nameEmptyError = document.querySelector(".name-empty-error")
+const nameSmallError = document.querySelector(".name-small-error")
+
+// submitting name 
+nameSubmit.addEventListener("click", () => {
+    if (nameInput.value.length > 2) {
+        username.innerHTML = nameInput.value + " is chatting"
+    } 
+    socket.emit("nameset", nameInput.value)
+})
+
+socket.on("nameSmallError", () => {
+    nameSmallError.style.opacity = 1
+    nameEmptyError.style.opacity = 0
+})
+
+socket.on("nameEmptyError", () => {
+    nameEmptyError.style.opacity = 1
+    nameSmallError.style.opacity = 0
+})
+
+
+// recieving namesetdone event from backend after name is set
+socket.on("namesetdone", () => {
+    nameScreen.style.display = "none"
+})
+
+socket.on("numberofliveusers", (liveUsers)=> {
+    if(liveUsers === 1) {
+        document.querySelector(".live-users").innerHTML = liveUsers + " person is live"
+    } 
+    else {
+        document.querySelector(".live-users").innerHTML = liveUsers + " people are live"
+    }
+})
+
+// writing a global function to send message 
+function sendMessage() {
+    const inputValue = input.value.trim();
+    if (inputValue) {
+        socket.emit("message", inputValue);
+        input.value = "";
+    }
+}
+
+// sending the message through send button 
+sendButton.addEventListener("click", function () {
+    sendMessage()
+});
+
+// displaying the message in message window through sending a socket event
+
+let container = ``;
+// Assuming you have a WebSocket instance named `socket`
+// Also assuming `messages` is a reference to a DOM element where messages will be displayed
+
+socket.on("message", function (message) {
+    let myMessage = message.id === socket.id;
+    const container = `
+        <div class="flex ${myMessage ? 'justify-end' : 'justify-start'}">
+            <div class="${myMessage ? 'bg-blue-600' : 'bg-zinc-800'} text-white p-3 ${myMessage ? 'rounded-l-lg rounded-br-lg' : 'rounded-r-lg rounded-tl-lg'}">
+                <p class="text-lg">
+                    <span class="font-bold">${message.name}</span>: <span class="font-light">${message.message}</span>
+                </p>
+            </div>
+        </div>
+    `;
+
+    // Append the constructed message to the messages container
+    messageDiv.innerHTML += container;
+
+    // Update the scroll position of the messages container to ensure the latest message is visible
+    messages.scrollTop = messages.scrollHeight;
+});
+
+
+
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        if (event.shiftKey) {
+            const cursorPos = this.selectionStart;
+            this.value = this.value.slice(0, cursorPos) + "\n" + this.value.slice(cursorPos);
+            // Move the cursor position just after the inserted newline
+            this.selectionStart = this.selectionEnd = cursorPos + 1;
+        } else {
+            event.preventDefault();
+            socket.emit("message", input.value);
+            input.value = "";
+        }
+    }
+});
